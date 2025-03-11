0ee4df908051eedfe4218644eae10ff65d7ca59b
eric_guim <eric.guimaraes07@gmail.com>
2025-03-11 13:33:02 -0300

    adjusting everything to run inside a docker image - removing .env from committed files

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { PrismaService } from '../prisma/prisma.service';
 @Module({
   imports: [],
-  controllers: [UserController],
+  controllers: [],
   providers: [UserService, UserRepository, PrismaService],
   exports: [UserService],
 })
